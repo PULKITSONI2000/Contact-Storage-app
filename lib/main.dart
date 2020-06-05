8cf3d369ab2cafb4329c6ce97c998320b7c21e08
Pulkit Soni <pulkitsoni2000@gmail.com>
2020-06-05 17:40:30 +0530

    Finish

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:contact_storage_app/Screens/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contact Storeage App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.lightBlueAccent,
+      ),
+      home: HomePage(),
+    );
+  }
+}
